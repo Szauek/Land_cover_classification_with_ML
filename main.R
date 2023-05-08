f19a8d3e2fd8bbaee28acbdfeb185df77d9b68c4
szauek <adrian.szalkowskil@gmail.com>
2023-05-09 00:30:49 +0200

    Added model 20 and model 40
    Comparing two models and  small fixes included

--- main.R
+++ main.R
@@ -7,16 +7,11 @@ library(sf)
 library(CAST)
 library(tmap)
 library(terra)
+library(gridExtra)
 
 # Dane pobrane z EO Browser, Sentinel 2 o danych (...), Utworzono raster stack
 #
 main_data <- stack("data/raster_stack.grd")
-
-
-#usunac
-rgbplot_main_data <- spplot(main_data[[1]],  col.regions="transparent",
-                     sp.layout =rgb2spLayout(sen_ms[[3:1]], quantiles = c(0.02, 0.98), alpha = 1))
-??rgb2spLayout
 
 # Sprawdzmy jak przedstawiaja sie dane
 plot(main_data)
@@ -24,75 +19,129 @@ plotRGB(main_data, stretch ="lin", r = 3, g = 2, b = 1)
 
 #nalezy wczytac warstwe z poligonami, ktore posluza jako warstwy treningowe
 
-trainSites <- read_sf("data/trainFields2.gpkg")
-print(trainSites)
+trainSites40 <- read_sf("data/trainFields2.gpkg")
+print(trainSites40)
+
+trainSites20 <- read_sf("data/trainFields.gpkg")
+print(trainSites20)
 
 viewRGB(main_data, r = 3, g = 2, b = 1, map.types = "Esri.WorldImagery")+
-  mapview(trainSites)
+  mapview(trainSites40)
 
-extr <- extract(main_data, trainSites, df=TRUE)
-
+extr40 <- extract(main_data, trainSites40, df=TRUE)
+extr20 <- extract(main_data, trainSites20, df=TRUE)
 #head(extr$ID)
-
-extr <- merge(extr, trainSites, by.x="ID", by.y="ID")
-head(extr)
+extr4
+extr40 <- merge(extr40, trainSites40, by.x="ID", by.y="ID")
+extr20 <- merge(extr20, trainSites20, by.x="ID", by.y="ID")
+head(extr40)
 
 set.seed(100)
-trainids <- createDataPartition(extr$ID,list=FALSE,p=0.05)
-trainDat <- extr[trainids,]
+trainids40 <- createDataPartition(extr40$ID,list=FALSE,p=0.1) #CARET
+trainDat40 <- extr40[trainids40,]
+
+trainids20 <- createDataPartition(extr20$ID,list=FALSE,p=0.1) #CARET
+trainDat20 <- extr20[trainids20,]
 # metoda cv czyli cross validacja, sluzy do lepszego przewidywania zmian
 # zamiast na wszystkich zmiennych, model uczy się jedynie na polu treningowym
 # Zapewnia większą pewność, by model lepiej pracował
 predictors <- names(main_data)
 response <- "typ"
-indices <- CreateSpacetimeFolds(trainDat,spacevar = "ID",k=3,class="typ")
+indices40 <- CreateSpacetimeFolds(trainDat40,spacevar = "ID",k=3,class = "typ") #CAST
 ctrl <- trainControl(method="cv", 
-                     index = indices$index,
+                     index = indices40$index,
+                     savePredictions = TRUE)
+
+indices20 <- CreateSpacetimeFolds(trainDat20,spacevar = "ID",k=3,class="typ") #CAST
+ctrl <- trainControl(method="cv", 
+                     index = indices20$index,
                      savePredictions = TRUE)
 #View(extr)
 
 # train the model
-model <- train(trainDat[,predictors],
-               trainDat[,response],
+model40 <- train(trainDat40[,predictors],
+               trainDat40[,response],
                method="rf",
                ntree=75)
-model
+model40
+print(model40)
+plot(model40)
+plot(varImp(model40))
 
-model <-  train(trainDat[,predictors],trainDat[,response])
+model20 <- train(trainDat20[,predictors],
+                 trainDat20[,response],
+                 method="rf",
+                 ntree=75)
+model20
+print(model20)
+plot(model20)
+plot(varImp(model20))
+
+model40 <-  train(trainDat40[,predictors],trainDat40[,response])
 
 set.seed(100)
-model <- ffs(trainDat[,predictors],
-             trainDat[,response],
+model40 <- ffs(trainDat40[,predictors],
+             trainDat40[,response],
              method="rf",
              metric="Kappa",
              trControl=ctrl,
              importance=TRUE,
              ntree=75)
 
-print(model)
-plot(model)
-plot(varImp(model))
+print(model40)
+plot(model40)
+plot(varImp(model40))
 
-#proba przewidzenia
-cvPredictions <- model$pred[model$pred$mtry==model$bestTune$mtry,]
-table(cvPredictions$pred,cvPredictions$obs)
+model20 <-  train(trainDat20[,predictors],trainDat20[,response])
+
+set.seed(100)
+model20 <- ffs(trainDat20[,predictors],
+               trainDat20[,response],
+               method="rf",
+               metric="Kappa",
+               trControl=ctrl,
+               importance=TRUE,
+               ntree=75)
+
+print(model20)
+plot(model20)
+plot(varImp(model20))
+
+# proba przewidzenia
+cvPredictions40 <- model40$pred[model40$pred$mtry==model40$bestTune$mtry,]
+table(cvPredictions40$pred,cvPredictions40$obs)
 # 
-prediction <- predict(main_data,model)
+cvPredictions20 <- model20$pred[model20$pred$mtry==model20$bestTune$mtry,]
+table(cvPredictions20$pred,cvPredictions20$obs)
+
+prediction40 <- predict(main_data,model40)
+prediction20 <- predict(main_data,model20)
 cols <- rev(c("palegreen", "grey", "blue", "forestgreen", "brown","beige","yellowgreen"))
-AOA <- aoa(main_data,model)
-tm_shape(prediction) +
-  tm_raster(palette = cols,title = "LUC")+
+AOA <- aoa(main_data,model40)
+tm_shape(prediction40) +
+  tm_raster(palette = cols,title = "Legenda")+
   tm_scale_bar(bg.color="white",bg.alpha=0.75)+
   tm_layout(legend.bg.color = "white",
             legend.bg.alpha = 0.75)
 
-AOA <- aoa(main_data,model)
+tm_shape(prediction20) +
+  tm_raster(palette = cols,title = "Legenda")+
+  tm_scale_bar(bg.color="white",bg.alpha=0.75)+
+  tm_layout(legend.bg.color = "white",
+            legend.bg.alpha = 0.75)
+
 plot(AOA)
 plot(AOA$AOA)
 plot(AOA$DI)
 plot(AOA$parameters)
 
-predplot <- spplot(deratify(predictions),col.regions=cols, main = list(label="Prediction (left), prediction only for the AOA (right) and RGB composite (bottom)",cex=0.8))
-predplotaoa <- spplot(deratify(prediction),col.regions=cols)+
+predplot40 <- spplot(deratify(prediction40),col.regions=cols, main = list(label="Prediction 40",cex=0.8))
+predplot20 <- spplot(deratify(prediction20),col.regions=cols, main = list(label="Prediction 20",cex=0.8))
+
+predplotaoa <- spplot(deratify(prediction40),col.regions=cols)+
 spplot(AOA$AOA,col.regions=c("grey", "transparent"))
-latticeExtra::CombineGrid(list(predplot,predplotaoa,rgbplot_main_data), layout=c(2,2))
+
+plot(predplot)
+plot(predplotaoa)
+
+grid.arrange(predplot40,predplot20)
